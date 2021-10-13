673596f9a6d32f929918c85ae0368bc93770402f
Elias Nijs <elias.nijs@gmail.com>
2021-10-13 17:04:54 +0200

    movement and collisions work now

--- src/Main.hs
+++ src/Main.hs
@@ -6,7 +6,6 @@ import Graphics.Gloss.Interface.Pure.Game
 type X = Int
 
 type Y = Int
-
 type Coord = (X, Y)
 
 type Line = [X]
@@ -24,9 +23,9 @@ data Game
 backgroundColor, activeColor, inactiveColor :: Color
 backgroundColor = makeColor 0 0 0 1
 
-activeColor = makeColor 1 1 1 1
+activeColor = makeColor 0 1 1 1
 
-inactiveColor = makeColor 0.2 0.2 0.2 1
+inactiveColor = makeColor 0 0.12 0.12 1
 
 -- Enkele constanten om te gebruiken in de rest van de code. Bij het
 -- quoteren kunnen wij deze veranderen om te kijken welke invloed ze
@@ -49,7 +48,7 @@ dblock = 12 -- de zijde van 1 blokje (original: 12)
 
 dwidth = 10 -- de zijde van 1 blokje (original: 10)
 
-dinner = 7 -- de zijde van 1 blokje (original: 7)
+dinner = 3 -- de zijde van 1 blokje (original: 7)
 
 fscale = 3 -- algemene schaal van de hele tekening (original: 3)
 
@@ -67,9 +66,9 @@ right = div width 2
 -- GRAFISCHE ELEMENTEN ------------------------------------------------
 pixel :: Color -> Picture
 pixel c =
-  let outer = color c $ rectangleWire (fscale * dwidth) (fscale * dwidth)
-      inner = color c $ rectangleSolid (fscale * dinner) (fscale * dinner)
-   in pictures [outer, inner]
+  let outer = rectangleWire (fscale * dwidth) (fscale * dwidth)
+      inner = rectangleSolid (fscale * dinner) (fscale * dinner)
+   in color c $ pictures [outer, inner]
 
 filledPixel :: Picture
 filledPixel = pixel activeColor
@@ -88,7 +87,8 @@ drawXAt p c = uncurry translate (gridToviewCoords c) p
 
 emptyBoard :: Picture
 emptyBoard =
-  pictures [drawXAt emptyPixel (x, y) | x <- [left .. right], y <- [bottom .. top]]
+  pictures
+    [drawXAt emptyPixel (x, y) | x <- [left .. right], y <- [bottom .. top]]
 
 drawCoord :: Coord -> Picture
 drawCoord = drawXAt filledPixel
@@ -114,55 +114,86 @@ onBoard c =
 atBottom :: Coord -> Bool
 atBottom c = snd c == bottom
 
--- Gegeven twee lijsten van coördinaten, geef deze twee lijsten terug
--- zonder de coördinaten die ze gemeenschappelijk hebben.
 collide :: [Coord] -> [Coord] -> ([Coord], [Coord])
-collide c1 c2 =
-  let its = c1 `intersect` c2
-   in (c1 \\ its, c2 \\ its)
+collide c1 c2 = (c1 \\ c2, c2 \\ c1)
 
--- Gebruik de `move` functie om de coördinaten in `moving` te verzetten.
--- Bij botsingen met de coördinaten in `static` moeten beide coördinaten
--- verwijderd worden uit de teruggegeven lijsten.
---TODO (elias):
-moveAndCollide :: (Coord -> Coord) -> ([Coord], [Coord]) -> ([Coord], [Coord])
-moveAndCollide move (moving, static) = undefined
-
---TODO (elias):
-nextFrame :: Float -> Game -> Game
-nextFrame t = undefined
-
--- Hulpfuncties om een getal te decrementeren of incrementeren zonder
--- een grens te overschrijden.
 decBound, incBound :: (Ord a, Num a) => a -> a -> a
 decBound x b = max b (x - 1)
 
 incBound x b = min b (x + 1)
 
---TODO (elias):
-move :: Event -> Game -> Game
-move (EventKey (SpecialKey KeyLeft) Down _ _) = undefined
-move (EventKey (SpecialKey KeyRight) Down _ _) = undefined
-move (EventKey (SpecialKey KeySpace) Down _ _) = undefined
-move _ = undefined
+moveAndCollide :: (Coord -> Coord) -> ([Coord], [Coord]) -> ([Coord], [Coord])
+moveAndCollide move (moving, static) = collide [move c | c <- moving] static
 
+-- TODO (elias): M : fix no lines left
+nextFrame :: Float -> Game -> Game
+nextFrame t (Playing p o b l) =
+   let t = moveAndCollide (\c -> (fst c, decBound (snd c) bottom)) (o, b)
+       q = moveAndCollide (\c -> (fst c, incBound (snd c) top)) (snd t, fst t)
+    in Playing
+    p
+    (snd q ++ [(e - 5, top) | e <- head l])
+    (fst q)
+    (tail l)
+
+move :: Event -> Game -> Game
+move (EventKey (SpecialKey KeyLeft) Down _ _) (Playing p o b l) =
+  Playing (decBound (fst p) left, snd p) o b l
+move (EventKey (SpecialKey KeyRight) Down _ _) (Playing p o b l) =
+  Playing (incBound (fst p) right, snd p) o b l
+move (EventKey (SpecialKey KeySpace) Down _ _) (Playing p o b l) =
+  Playing p o (b ++ [(fst p, incBound (snd p) top)]) l
+move _ game = game
+
+-- ADDITIONAL FEATURES --------------------------------------------------
+-- TODO (elias): M : delay between shooting
+-- TODO (elias): M : particle system
+-- TODO (elias): M : camera shake
+-- TODO (elias): M : better feedback on: dieing, shooting and killing 
+-- TODO (elias): M : random
 -- MAIN -----------------------------------------------------------------
 level1 :: [Line]
-level1 = [[0, 1, 3, 4, 5, 9, 10], [], [], [], [2, 3, 4, 5, 6, 7, 9, 10]]
+level1 =
+  [ [0, 1, 3, 4, 5, 9, 10]
+  , []
+  , []
+  , []
+  , [2, 3, 4, 5, 6, 7, 9, 10]
+  , []
+  , []
+  , []
+  , []
+  , []
+  , []
+  , []
+  , []
+  , []
+  , []
+  , []
+  , []
+  , []
+  , []
+  , []
+  , []
+  , []
+  , []
+  , []
+  , []
+  ]
 
 startGame = Playing (0, -10) [] [] level1
 
--- main =
---   play
---     (InWindow "Brick Game (c) Elias Nijs" (500, 800) (10, 10))
---     backgroundColor
---     2 -- aantal stappen per seconde
---     startGame
---     renderPic
---     move
---     nextFrame
 main =
-  display
+  play
     (InWindow "Brick Game (c) Elias Nijs" (500, 800) (10, 10))
     backgroundColor
-    (renderPic startGame)
+    2 -- aantal stappen per seconde
+    startGame
+    renderPic
+    move
+    nextFrame
+-- main =
+--   display
+--     (InWindow "Brick Game (c) Elias Nijs" (500, 800) (10, 10))
+--     backgroundColor
+--     (renderPic startGame)
