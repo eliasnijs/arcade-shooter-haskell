b9c64a970d5a83b605cb755be332c73bc1eb1df8
Elias Nijs <elias.nijs@gmail.com>
2021-10-16 20:39:05 +0200

    implemented images for the player, bullets and enemies + code cleanup

--- src/Main.hs
+++ src/Main.hs
@@ -11,12 +11,15 @@ type Coord = (X, Y)
 
 type Line = [X]
 
+type Texture = Picture
+
 data Game
   = Playing
       { player :: Coord
       , obstacles :: [Coord]
       , bullets :: [Coord]
       , lines :: [Line]
+      , textures :: [Texture]
       }
   | GameOver
   | Won
@@ -65,54 +68,43 @@ left = div (-width) 2
 right = div width 2
 
 --- RENDERER -----------------------------------------------------------
-pixel :: Color -> Picture
-pixel c =
-  let outer = rectangleWire (fscale * dwidth) (fscale * dwidth)
-      inner = rectangleSolid (fscale * dinner) (fscale * dinner)
-   in color c $ pictures [outer, inner]
-
-emptyPixel :: Picture
-emptyPixel = pixel inactiveColor
-
-enemyPixel :: Picture
-enemyPixel = pixel enemyColor
-
-playerPixel :: Picture
-playerPixel = pixel playerColor
-
-bulletPixel :: Picture
-bulletPixel =
-  let inner = rectangleSolid (fscale * dinner) (fscale * dinner)
-   in color bulletColor $ pictures [inner]
-
 gridToviewCoords :: Coord -> (Float, Float)
 gridToviewCoords c =
   let x = fromIntegral $ fst c :: Float
       y = fromIntegral $ snd c :: Float
    in (x * dblock * fscale, y * dblock * fscale)
 
-emptyBoard :: Picture
-emptyBoard =
-  pictures
-    [drawXAt emptyPixel (x, y) | x <- [left .. right], y <- [bottom .. top]]
-
 drawXAt :: Picture -> Coord -> Picture
 drawXAt p c = uncurry translate (gridToviewCoords c) p
 
+scaleImage :: Picture -> Picture
+scaleImage = scale fscale fscale
+
+pixel :: Color -> Picture
+pixel c =
+  let outer = rectangleWire (fscale * dwidth) (fscale * dwidth)
+      inner = rectangleSolid (fscale * dinner) (fscale * dinner)
+   in color c $ pictures [outer, inner]
+
+emptyBoard :: Picture
+emptyBoard =
+  pictures
+    [drawXAt (pixel inactiveColor) (x, y) | x <- [left .. right], y <- [bottom .. top]]
+
 renderPic :: Game -> Picture
-renderPic (Playing p o b _) =
+renderPic (Playing p o b _ t) =
   pictures
     [ emptyBoard
-    , drawXAt playerPixel p
-    , pictures [drawXAt enemyPixel c | c <- o]
-    , pictures [drawXAt bulletPixel c | c <- b]
+    , pictures $ fmap (drawXAt $ scaleImage $ t !! 1) o 
+    , pictures $ fmap (drawXAt $ scaleImage $ head t) b
+    , drawXAt (scaleImage $ t !! 2) p
     ]
 renderPic Won =
   pictures
-    [drawXAt playerPixel (x, y) | x <- [left .. right], y <- [bottom .. top]]
+    [drawXAt (pixel playerColor) (x, y) | x <- [left .. right], y <- [bottom .. top]]
 renderPic GameOver =
   pictures
-    [drawXAt enemyPixel (x, y) | x <- [left .. right], y <- [bottom .. top]]
+    [drawXAt (pixel enemyColor) (x, y) | x <- [left .. right], y <- [bottom .. top]]
 
 -- ENGINE --------------------------------------------------------------
 onBoard :: Coord -> Bool
@@ -122,24 +114,29 @@ onBoard c =
    in x >= left && x <= right && y >= bottom && y <= top
 
 atBottom :: Coord -> Bool
-atBottom c = snd c == bottom
+atBottom (x, y) = y == bottom
 
 collide :: [Coord] -> [Coord] -> ([Coord], [Coord])
 collide c1 c2 = (c1 \\ c2, c2 \\ c1)
 
 moveAndCollide :: (Coord -> Coord) -> ([Coord], [Coord]) -> ([Coord], [Coord])
 moveAndCollide move (moving, static) =
-  collide [move c | c <- moving, onBoard (move c)] static
+  collide (filter onBoard (fmap move moving)) (nub static)
 
 nextFrame :: Float -> Game -> Game
-nextFrame t (Playing p o b ln)
+nextFrame t (Playing p o b ln tex)
   | any atBottom o = GameOver
   | all null o && all null ln = Won
   | otherwise =
     let t = moveAndCollide (\c -> (fst c, snd c - 1)) (o, b)
         q = moveAndCollide (\c -> (fst c, snd c + 1)) (snd t, fst t)
         l = ln ++ [[]]
-     in Playing p (snd q ++ [(e - right, top) | e <- head l]) (fst q) (tail l)
+     in Playing
+          p
+          (snd q ++ [(e - right, top) | e <- head l])
+          (fst q)
+          (tail l)
+          tex
 nextFrame t g = g
 
 decBound, incBound :: (Ord a, Num a) => a -> a -> a
@@ -148,25 +145,35 @@ decBound x b = max b (x - 1)
 incBound x b = min b (x + 1)
 
 move :: Event -> Game -> Game
-move (EventKey (SpecialKey KeyLeft) Down _ _) (Playing p o b l) =
-  Playing (decBound (fst p) left, snd p) o b l
-move (EventKey (SpecialKey KeyRight) Down _ _) (Playing p o b l) =
-  Playing (incBound (fst p) right, snd p) o b l
-move (EventKey (SpecialKey KeySpace) Down _ _) (Playing p o b l) =
-  Playing p o (b ++ [(fst p, snd p + 1)]) l
+move (EventKey (SpecialKey KeyLeft) Down _ _) (Playing p o b l t) =
+  Playing (decBound (fst p) left, snd p) o b l t
+move (EventKey (SpecialKey KeyRight) Down _ _) (Playing p o b l t) =
+  Playing (incBound (fst p) right, snd p) o b l t
+move (EventKey (SpecialKey KeySpace) Down _ _) (Playing p o b l t) =
+  Playing p o (b ++ [p]) l t
 move _ game = game
 
 --- MAIN ---------------------------------------------------------------
 level1 :: [Line]
 level1 = [[0, 1, 3, 4, 5, 9, 10], [], [], [], [2, 3, 4, 5, 6, 7, 9, 10]]
 
-startGame = Playing (0, -10) [] [] level1
-
-main =
+main :: IO ()
+main = do
+  bulletTexture <- loadBMP "resources/bullet.bmp"
+  enemyTexture <- loadBMP "resources/enemy.bmp"
+  playerTexture <- loadBMP "resources/player.bmp"
+  let startGame =
+        Playing
+          (0, bottom)
+          []
+          []
+          level1
+          [bulletTexture, enemyTexture, playerTexture]
+  bulletImage <- loadBMP "resources/bullet.bmp"
   play
     (InWindow "Brick Game (c) Elias Nijs" (500, 800) (10, 10))
     backgroundColor
-    2 -- aantal stappen per seconde
+    1 -- aantal stappen per seconde
     startGame
     renderPic
     move
